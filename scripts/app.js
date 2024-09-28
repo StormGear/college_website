4908c02939023db3e1d4f771715d554ed57c0e42
Papa Kofi Boahen <papakofiboahen@gmail.com>
2024-09-28 07:46:57 +0000

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,5 @@
+// if (window.innerWidth < 350) {
+//     document.querySelector('.material-symbols-outlined').style.display = 'block';
+// } else {
+//     document.querySelector('.material-symbols-outlined').style.display = 'none';
+// }

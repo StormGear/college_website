eea68d04aea10d16c34ab51d75743a394112f230
Papa Kofi Boahen <papakofiboahen@gmail.com>
2024-09-28 14:58:10 +0000

    hero and courses section complete

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,22 @@
-// if (window.innerWidth < 350) {
-//     document.querySelector('.material-symbols-outlined').style.display = 'block';
-// } else {
-//     document.querySelector('.material-symbols-outlined').style.display = 'none';
-// }
+let navLinks = document.querySelector('.nav-links');
+
+function showMenu() {
+  navLinks.style.right = '0';
+  console.log('showing menu');
+}
+function hideMenu() {
+  navLinks.style.right = '-200px';
+  console.log('hiding menu');
+}
+
+document.addEventListener('click', function(event) {
+    // Check if the click target is outside the navLinks div
+    if (event.target.innerText === "menu") {
+      showMenu();
+    } 
+    else if (!navLinks.contains(event.target)) {
+    hideMenu();
+    }
+  });
+
+console.log(navLinks);
